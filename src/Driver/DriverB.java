b66e6bbc53dae04a026e2d56a0d980ba204cd7cb
Пользователь <woodoo08@mail.ru>
2022-11-18 04:22:13 +0300

    Урок 4. Полиморфизм.

--- src/Driver/DriverB.java
+++ src/Driver/DriverB.java
@@ -2,7 +2,7 @@ package Driver;
 
 import Transport.Car;
 
-public class DriverB extends Drivers<Car>{
+public class DriverB extends Drivers<Car> {
 
     public DriverB(String fullName, String driverLicence, int experience, Car car) {
         super(fullName, driverLicence, experience, car);
--- src/Transport/Transport.java
+++ src/Transport/Transport.java
@@ -39,6 +39,7 @@ public abstract class Transport {
     }
 
     public abstract void startMoving();
+
     public abstract void stopMoving();
 
     @Override
